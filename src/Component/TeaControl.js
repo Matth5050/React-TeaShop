e9e03cae5e9573efdf486a48cdeffa2bbf5cc9fe
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-09 09:52:21 -0700

    added new render section with basic conditional for which component is currently shown

--- src/Component/TeaControl.js
+++ src/Component/TeaControl.js
@@ -1,4 +1,6 @@
 import React from "react";
+import TeaList from "./TeaList";
+import NewTeaForm from "./NewTeaForm";
 
 
 class TeaControl extends React.Component {
@@ -9,7 +11,22 @@ class TeaControl extends React.Component {
       formVisibleOnPage: false
     };
   }
-  
+
+  render() {
+    let currentlyVisibleState = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTeaForm />
+    } else {
+      currentlyVisibleState = <TeaList />
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+      </React.Fragment>
+    );
+  }
 
 
 }
+
+export default TeaControl;
