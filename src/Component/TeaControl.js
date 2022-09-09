f62994fa5f03f415ff1590482245004a7ed263ff
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-09 10:47:43 -0700

    added new event handlers

--- src/Component/TeaControl.js
+++ src/Component/TeaControl.js
@@ -8,20 +8,50 @@ class TeaControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      mainTeaList: [],
+      selectedTea: null,
+      editing: false
     };
   }
 
+  handleClick = () => {
+    if (this.state.selectedTea != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedTea: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
+    }
+  }
+
+  handleAddingNewTeaToList = (newTea) => {
+    const newMainTeaList = this.state.mainTeaList.concat(newTea);
+    this.setState({mainTeaList: newMainTeaList,
+                  formVisibleOnPage: false });
+  }
+
+
+
   render() {
     let currentlyVisibleState = null;
+    let buttonText = null;
+    
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTeaForm />
+      currentlyVisibleState = <NewTeaForm onNewTeaCreation={this.handleAddingNewTeaToList} />
+      buttonText = "Return to Item List";
     } else {
-      currentlyVisibleState = <TeaList />
+      currentlyVisibleState = <TeaList teaList={this.state.mainTeaList} />
+      buttonText = "Add Tea";
     }
     return (
       <React.Fragment>
         {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button> 
       </React.Fragment>
     );
   }
