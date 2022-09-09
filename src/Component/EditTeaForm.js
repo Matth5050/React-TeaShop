1832c385d34967e26c37a53df4a95f48f53b85b3
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-09 14:34:52 -0700

    added edit team form

---
+++ src/Component/EditTeaForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+function EditTeaForm (props) {
+
+  const { tea } = props;
+
+  function handleEditTeaFormSubmission(event) {
+    event.preventDefault();
+    props.onEditTea({name: event.target.name.value, origin: event.target.origin.value, price: event.target.price.value, steep: event.target.steep.value, id: tea.id});
+  }
+  return (
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler={handleEditTeaFormSubmission}
+        buttonText="Update Tea" />
+    </React.Fragment>
+  );
+}
+
+EditTeaForm.propTypes = {
+  tea: PropTypes.object,
+  onEditTea: PropTypes.func
+};
+
+export default EditTeaForm;
