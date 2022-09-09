b1ba92476ea39fdd03b944f65294626c6b67d010
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-09 15:29:52 -0700

    added code for counter

--- src/Component/TeaDetail.js
+++ src/Component/TeaDetail.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function TeaDetail(props){
-  const { tea, onClickingDelete } = props;
+  const { tea, onClickingDelete, counter } = props;
 
   return (
     <React.Fragment>
@@ -10,6 +10,8 @@ function TeaDetail(props){
       <h3>{tea.name} - {tea.origin}</h3>
       <p><em>{tea.price}</em></p>
       <p><em>{tea.steep}</em></p>
+      <p><em>{counter}</em></p>
+      <button onClick={props.onRestockClick}>Restock Tea</button>
       <button onClick={props.onClickingEdit}>Edit Tea</button>
       <button onClick={()=> onClickingDelete(tea.id) }>Delete Tea</button>
       <hr/>
@@ -20,7 +22,8 @@ function TeaDetail(props){
 TeaDetail.propTypes = {
   tea: PropTypes.object,
   onClickingDelete: PropTypes.func,
-  onClickingEdit: PropTypes.func
+  onClickingEdit: PropTypes.func,
+  onRestockClick: PropTypes.func
 };
 
 export default TeaDetail;
